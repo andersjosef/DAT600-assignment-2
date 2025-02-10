87f0f8b89af07c1e8017f285d817b44cb98c1d58
Anders <a-tunglan@hotmail.com>
2025-02-10 14:33:12 +0100

    problemgenerator done

--- task2/KnapSackProblem.java
+++ task2/KnapSackProblem.java
@@ -2,24 +2,54 @@ import java.util.ArrayList;
 import java.util.Random;;
 
 class KnapSackProblem {
-    private ArrayList<Integer> items = new ArrayList<Integer>();
-    Random rand = new Random(42);
+    private ArrayList<BagEntry> items = new ArrayList<BagEntry>();
+    // Seed random for consistency
+    Random rand = new Random(42); 
 
 
-    KnapSackProblem(int numberOfItems, int minValue, int maxValue) {
+    KnapSackProblem(int numberOfItems, int minValue, int maxValue, int minWeight, int maxWeight) {
         for (int i = 0; i<numberOfItems; i++) {
-            items.add(rand.nextInt(minValue, maxValue));
+            // Generate new bagentry with weigth and value
+            BagEntry newBagEntry = new BagEntry(
+                rand.nextInt(minWeight, maxWeight),
+                rand.nextInt(minValue, maxValue)
+            );
+            items.add(newBagEntry);
+
         }
         
     }
 
     public String toString() {
-        String toBeReturned = "item : value\n";
+        String toBeReturned = "i : weight : value\n";
         for (int i = 0; i<items.size(); i++) {
            toBeReturned += i + " : " + items.get(i) + "\n";
         }
 
         return toBeReturned;
     }
+}
 
+// One entry with weight and value
+class BagEntry {
+    private int weight;
+    private int value;
+
+    BagEntry(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public String toString() {
+        return this.weight + " : " + this.value;
+    }
 }
--- task2/Main.java
+++ task2/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        KnapSackProblem ksProblem = new KnapSackProblem(10, 5, 100);
+        KnapSackProblem ksProblem = new KnapSackProblem(10, 5, 100, 1, 100);
         System.out.println(ksProblem.toString());
         KnapSackSolver.BinaryKnapSack();
         KnapSackSolver.FractionalKnapSack();
